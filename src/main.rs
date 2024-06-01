b172ad58c414aefb758486620bc8e749855569f8
Kacper Szot <kacperszot1@gmail.com>
2024-06-01 18:53:09 +0200

    add templating engine

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::{get, launch, routes};
+use rocket::fs::{FileServer, Options, relative};
+use rocket_dyn_templates::{context, Template};
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        // add templating system
+        .attach(Template::fairing())
+        // serve content from disk
+        .mount("/public", FileServer::new(relative!("/public"), Options::Missing | Options::NormalizeDirs))
+        // register routes
+        .mount("/", routes![root])
+}
+
+#[get("/")]
+async fn root() -> Template {
+    Template::render("root", context! { message: "Hello, Rust"})
 }
